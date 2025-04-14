2c98208885da9c44a507a1926d79239be47bc264
Israel Fagundes <dev.israelfagundes@gmail.com>
2025-04-14 19:26:27 -0300

    feat: add routes group

--- router/router.go
+++ router/router.go
@@ -3,11 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Init() {
+	// Initialize Router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Initialize Routes
+	initRoutes(router)
+
+	// Run the server
 	router.Run()
 }

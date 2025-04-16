7b977cfaea988d1a30ebd9efdf0e5859d48cbe13
Israel Fagundes <dev.israelfagundes@gmail.com>
2025-04-15 22:21:46 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/israelfagundes/gopportunities/router"
+import (
+	"github.com/israelfagundes/gopportunities/config"
+	"github.com/israelfagundes/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Init()
 }
